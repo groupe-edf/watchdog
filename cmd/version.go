0f7cc0ad9f7698ce674248b19010d8a6d1aa0dd5
Habib MAALEM <habib.maalem@gmail.com>
2020-12-01 18:24:17 +0100

    chore: print version short format

--- cmd/version.go
+++ cmd/version.go
@@ -12,19 +12,27 @@ const (
 	defaultVersion = "develop"
 )
 
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of watchdog",
-	Long:  `All software has versions. This is watchdog's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		buildInfo := version.GetBuildInfo()
-		if bi, ok := debug.ReadBuildInfo(); ok && buildInfo.Version == defaultVersion {
-			buildInfo.Version = bi.Main.Version
-		}
-		fmt.Println(string(buildInfo.ToJSON()))
-	},
-}
+var (
+	short      bool
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of watchdog",
+		Long:  `All software has versions. This is watchdog's`,
+		Run: func(cmd *cobra.Command, args []string) {
+			buildInfo := version.GetBuildInfo()
+			if bi, ok := debug.ReadBuildInfo(); ok && buildInfo.Version == defaultVersion {
+				buildInfo.Version = bi.Main.Version
+			}
+			if short {
+				fmt.Println(buildInfo.Version)
+			} else {
+				fmt.Println(string(buildInfo.ToJSON()))
+			}
+		},
+	}
+)
 
 func init() {
 	analyzeCommand.AddCommand(versionCmd)
+	versionCmd.Flags().BoolVar(&short, "short", false, "print just the version number.")
 }
